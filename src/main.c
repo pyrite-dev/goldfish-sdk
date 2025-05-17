009178cc64b36cb86b7d438088d8a8b749463966
NishiOwO <nishi@nishi.boats>
2025-05-18 05:55:04 +0900

    fix entrypoint

--- src/main.c
+++ src/main.c
@@ -3,7 +3,11 @@
 int	  scene;
 ui_area_t opengl_area;
 
+#ifdef _WIN32
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow){
+#else
 int main(int argc, char** argv) {
+#endif
 	sdk_ui_init();
 	sdk_ui_loop();
 }
